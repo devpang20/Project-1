8bbe1a8f78c4a2eb864800d88092c69520f8308a
sik2 <bickwang123@gmail.com>
2025-05-07 20:21:42 +0900

    DOM 작업

--- 6기 홍길동/script.js
+++ 6기 홍길동/script.js
@@ -1,1 +1,33 @@
-console.log("hi");
+// 연산자
+const $plus = document.querySelector("[data-btn-plus]");
+const $minus = document.querySelector("[data-btn-minus]");
+const $multiply = document.querySelector("[data-btn-multiply]");
+const $divide = document.querySelector("[data-btn-divide]");
+const $eqaul = document.querySelector("[data-btn-eqaul]");
+
+// AC, DEL
+const $reset = document.querySelector("[data-btn-reset]");
+const $delete = document.querySelector("[data-btn-delete]");
+
+// 숫자, 연산자
+const $numbers = document.querySelectorAll("[data-btn-number]");
+const $operations = document.querySelectorAll("[data-btn-operation]");
+
+// 프롬프트
+const $previousPreview = document.querySelector("[data-previous-preview]");
+const $currentPreview = document.querySelector("[data-current-preview]");
+
+console.log($numbers);
+// 숫자선택
+$numbers.forEach(($number) => {
+  $number.addEventListener("click", (e) => {
+    console.log(e.target);
+  });
+});
+
+// 연산자선택
+$operations.forEach(($operation) => {
+  $operation.addEventListener("click", (e) => {
+    console.log(e.target);
+  });
+});
