328bd131852d1b471dda2b4749b2a5ba3ac2ed40
sik2 <bickwang123@gmail.com>
2025-05-07 20:55:10 +0900

    onPress, Prompt

--- 6기 홍길동/script.js
+++ 6기 홍길동/script.js
@@ -1,3 +1,25 @@
+class Calualtor {
+  $previousPreviewPrompt = "";
+  $currentPreviewPrompt = "";
+  previousOperation = "";
+  currentOpenration = "";
+
+  constructor($previousPreview, $currentPreview) {
+    this.$previousPreviewPrompt = $previousPreview;
+    this.$currentPreviewPrompt = $currentPreview;
+  }
+
+  onPressNumber(number) {
+    this.$currentPreviewPrompt.textContent += number;
+  }
+
+  onPressOperation(operation) {
+    console.log(operation);
+  }
+}
+
+// 객체 생성
+
 // 연산자
 const $plus = document.querySelector("[data-btn-plus]");
 const $minus = document.querySelector("[data-btn-minus]");
@@ -17,17 +39,18 @@ const $operations = document.querySelectorAll("[data-btn-operation]");
 const $previousPreview = document.querySelector("[data-previous-preview]");
 const $currentPreview = document.querySelector("[data-current-preview]");
 
-console.log($numbers);
 // 숫자선택
 $numbers.forEach(($number) => {
   $number.addEventListener("click", (e) => {
-    console.log(e.target);
+    calc.onPressNumber(e.target.textContent);
   });
 });
 
 // 연산자선택
 $operations.forEach(($operation) => {
   $operation.addEventListener("click", (e) => {
-    console.log(e.target);
+    calc.onPressOperation($operation);
   });
 });
+
+const calc = new Calualtor($previousPreview, $currentPreview);
